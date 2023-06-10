681d13e6bb61d6bf4528999cc332a69a77c69f34
Marco Minaudo <32198621+BugNoFix@users.noreply.github.com>
2023-06-10 09:12:37 +0200

    added user details form

---
+++ src/app/components/user-details-form/user-details-form.component.ts
@@ -0,0 +1,42 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {UserBodyDetailsDTO} from "../../dto/user";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-user-details-form',
+  templateUrl: './user-details-form.component.html',
+  styleUrls: ['./user-details-form.component.css']
+})
+export class UserDetailsFormComponent {
+    @Output() onAddUser: EventEmitter<UserBodyDetailsDTO> = new EventEmitter();
+
+    weight!: number;
+    bodyfat!: number;
+    height!: number;
+    chest!: number;
+    upperArm!: number;
+    shoulders!: number;
+    waist!: number;
+    uploadTime!: Date;
+    constructor(private modalService: NgbModal) {}
+    open(content: any) {
+        this.modalService.open(content, { size: 'sm' }).result.then();
+    }
+
+    onSubmit(){
+
+        const userDetails: UserBodyDetailsDTO = {
+            weight: this.weight,
+            bodyfat: this.bodyfat,
+            height: this.height,
+            chest: this.chest,
+            upperArm: this.upperArm,
+            shoulders: this.shoulders,
+            waist: this.waist
+        };
+        console.log(userDetails);
+        this.onAddUser.emit(userDetails);
+
+    }
+
+}
